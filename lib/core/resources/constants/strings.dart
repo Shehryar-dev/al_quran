5f6e4ce0210d81a75ff4e954d1b4d0d5971ae203
Shehriyar Saleem <127856369+Shehryar-dev@users.noreply.github.com>
2025-03-21 20:05:45 +0500

    Update strings.dart

--- lib/core/resources/constants/strings.dart
+++ lib/core/resources/constants/strings.dart
@@ -3,7 +3,7 @@ class TextString {
   static final String bismillah = 'بِسۡمِ ٱللَّهِ ٱلرَّحۡمَٰنِ ٱلرَّحِيمِ';
 
 
-  static const String appTitle = 'Al-Quran Apps';  //
+  static const String appTitle = 'Al-Quran App';  //
 
   static const String introductionDescription = "Are you so busy that you haven't read\nthe Quran yet";
   static const String introductionBtn = 'GET STARTED';
